2ecf49b2051d7b15e8eb4be51905fd89a1248e83
Non0w <arnaud.cojez@etudiant.univ-lille1.fr>
2017-01-31 00:35:25 +0100

    add particles to control hud

--- sources/scripts/controls_hud.gd
+++ sources/scripts/controls_hud.gd
@@ -2,6 +2,8 @@ extends CanvasLayer
 
 onready var sprite_c1 = get_node("control_1_sprite")
 onready var sprite_c2 = get_node("control_2_sprite")
+onready var particles_c1 = sprite_c1.get_node("particles")
+onready var particles_c2 = sprite_c2.get_node("particles")
 
 func _ready():
 	pass
@@ -9,4 +11,6 @@ func _ready():
 
 func _on_player_controls_changed(player):
 	sprite_c1.play(player.control_1)
+	particles_c1.set_emitting(true)
 	sprite_c2.play(player.control_2)
+	particles_c2.set_emitting(true)
