b19eda343c1e698345a58eea2cab7a8a9c41e678
Non0w <arnaud.cojez@etudiant.univ-lille1.fr>
2017-03-03 22:50:54 +0100

    add game over menu

--- sources/scripts/levels/level.gd
+++ sources/scripts/levels/level.gd
@@ -6,16 +6,25 @@ signal restart
 onready var buttbutt = find_node("buttbutt")
 onready var camera = buttbutt.get_node("Camera2D")
 onready var menu_screen = find_node("menu_screen")
+onready var game_over_screen = find_node("game_over_screen")
 
 func _ready():
-	pass
+	reset()
 
 func reset():
 	buttbutt.reset()
 	camera.align()
+	
+	menu_screen.set_enabled(true)
+	menu_screen.close()
+	
+	game_over_screen.close()
+	game_over_screen.set_enabled(false)
 
 func _on_buttbutt_die():
-	reset()
+	menu_screen.set_enabled(false)
+	game_over_screen.set_enabled(true)
+	game_over_screen.open()
 
 func _on_buttbutt_exit():
 	emit_signal("menu")
@@ -29,4 +38,3 @@ func _on_menu_screen_menu():
 
 func _on_menu_screen_restart():
 	reset()
-	menu_screen._on_resume_button_pressed()
